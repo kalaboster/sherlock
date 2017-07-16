214309d18ebec98e9da6710530203188a5c1e292
Kalab Oster <kalaboster@gmail.com>
2017-07-15 23:03:56 -0700

    Copy methods from wordsum and put them here in this version of wordsum that I will change when I copy over. Publish because these should work, but I won't test right yet as I make my way to code actually new code.

--- src/main/java/com/wordsum/read/watson/utils/Publisher.java
+++ src/main/java/com/wordsum/read/watson/utils/Publisher.java
@@ -1,5 +1,10 @@
 package com.wordsum.read.watson.utils;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -11,6 +16,31 @@ import java.io.IOException;
  * .
  */
 public class Publisher {
+
+
+    protected Publisher(){}
+
+    /**
+     * A method that will take an object and print it in json in a defined path.
+     *
+     * @param objectState is the object to print ot a file as a json string.
+     * @param pathFile is the location to print the file.
+     * @throws IOException
+     */
+    public static void printPrettyObjectState(Object objectState, String pathFile) throws IOException {
+
+        //Print the pretty json to a file...
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonParser jsonParser = new JsonParser();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonElement jsonElement = jsonParser.parse(objectMapper.writeValueAsString(objectState));
+
+        String prettyJsonString = gson.toJson(jsonElement);
+
+        FileUtils.writeStringToFile(new File(pathFile), prettyJsonString);
+
+    }
 
 
 }
--- src/main/java/com/wordsum/read/watson/utils/Reader.java
+++ src/main/java/com/wordsum/read/watson/utils/Reader.java
@@ -1,7 +1,64 @@
 package com.wordsum.read.watson.utils;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by kalab on 7/15/17.
+ * A class that simply reads in a file and makes it a String.
+ *
  */
 public class Reader {
+
+
+    protected Reader(){}
+
+    /**
+     * This utility class is to make a string object to pass to some method that wants a string.
+     *
+     * @param path The path to the file to be read in as text.
+     * @return A String version of the txt file.
+     */
+    public static String readTxtFileToString(String path) {
+
+        String stringFile = null;
+
+        try {
+
+            File file = new File(path);
+
+            stringFile = FileUtils.readFileToString(file);
+
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return stringFile;
+
+    }
+
+
+    /**
+     * A method to serialize the sentence sin paragraphs of a file.
+     *
+     * @param fileString The string object that contains the text of a file.
+     * @return A list array for the sentences in the file.
+     */
+    public static List<String> serialSentences(String fileString){
+
+        List<String> list = new ArrayList<String>();
+
+
+
+        return list;
+    }
+
+
 }
